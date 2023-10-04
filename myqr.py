4086fd66790a973039590cba5ecd42c3472eb791
Mr. Error <122160056+101rror@users.noreply.github.com>
2023-10-04 10:01:27 +0600

    Delete myqr.py

--- myqr.py
+++
@@ -1,5 +0,0 @@
-import qrcode as qr
-
-myqr = qr.make("My Programming Profiles link :: \n\nhttps://auth.geeksforgeeks.org/user/101rror\nhttps://leetcode.com/101rror/\nhttps://codeforces.com/profile/101rror\nhttps://github.com/101rror")
-
-myqr.save("Myself.png")
